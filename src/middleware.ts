cbfd31789179d5912605466ff390625077155b36
Alireza Saremi <saremi.alireza@gmail.com>
2025-04-28 21:45:28 +0330

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    console.log("🔥 Middleware is running...");
-    console.log("Request URL:", req.url);  // See which URL is triggering
   
     return NextResponse.redirect(new URL("/login", req.url)); // Redirect on all routes
   }
   
   export const config = {
-    matcher: ["/*"],  // Match any route for testing
+    matcher: ["/dashboard/:path*"], 
   };
